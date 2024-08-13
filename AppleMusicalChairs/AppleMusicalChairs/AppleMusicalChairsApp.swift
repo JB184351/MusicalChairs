0e60a3fb23259268a9244c7bb6b361694699c83e
JB184351 <justinbengtson@icloud.com>
2024-08-13 15:30:43 -0700

    Initial Commit

---
+++ AppleMusicalChairs/AppleMusicalChairs/AppleMusicalChairsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicalChairsApp.swift
+//  AppleMusicalChairs
+//
+//  Created by Justin on 8/13/24.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicalChairsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
